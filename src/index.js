2bd6bc3a0aa6bfa46eea21eb63dd02b0516dd44f
alex-beaverg <a.beaverg@gmail.com>
2020-10-06 00:36:42 +0300

    docs: begin HRN task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,66 @@
 module.exports = function toReadable (number) {
-  
+    let result = '';
+    let nums;
+    let num;
+    switch (nums) {
+        case 1:
+            num = one;
+            break;
+        case 2:
+            num = two;
+            break;
+        case 3:
+            num = three;
+            break;
+        case 4:
+            num = four;
+            break;
+        case 5:
+            num = five;
+            break;
+        case 6:
+            num = six;
+            break;
+        case 7:
+            num = seven;
+            break;
+        case 8:
+            num = eight;
+            break;
+        case 9:
+            num = nine;
+            break;
+        case 10:
+            num = ten;
+            break;
+        case 11:
+            num = eleven;
+            break;
+        case 12:
+            num = twelve;
+            break;
+        case 13:
+            num = thirteen;
+            break;
+        case 14:
+            num = fourteen;
+            break;
+        case 15:
+            num = fifteen;
+            break;
+        case 16:
+            num = sixteen;
+            break;
+        case 17:
+            num = seventeen;
+            break;
+        case 18:
+            num = eighteen;
+            break;
+        case 19:
+            num = nineteen;
+            break;        
+    }
+
+    return result;
 }
